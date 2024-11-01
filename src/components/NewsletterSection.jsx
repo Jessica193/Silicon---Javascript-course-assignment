385a99a8bfcf297d3388c3274eb7ab71331a66b2
Jessica Eriksson <jessicahlqvist@hotmail.com>
2024-11-01 11:44:00 +0100

    validation on newsletter form done

--- src/components/NewsletterSection.jsx
+++ src/components/NewsletterSection.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import notification from '../assets/images/notification.svg'
 
 function NewsletterSection() {
+
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(email.trim() === '') {
+      console.log('You can not leave this field empty') /*Ändra senare*/
+        return
+      } 
+      else if (!regexEmail.test(email)) {
+        console.log('You have to enter a valid email-address') /*Ändra senare*/
+      }
+
+      setEmail('')
+
+    }
+
+
+
   return (
     <section className="newsletter-section">
         <div className="container container-newsletter">
@@ -12,8 +34,8 @@ function NewsletterSection() {
                 <h5 className="subscribe-h5-mobile">Subscribe to our newsletter</h5>
             </div>
 
-            <form className="newsletter-input">
-                <input type="email" name="email" placeholder="Your email"/>
+            <form onSubmit={handleSubmit} noValidate className="newsletter-input">
+                <input required value={email} onChange={(e) => {setEmail(e.target.value)}} type="email" name="email" placeholder="Your email"/>
                 <button className="btn-primary btn-subscribe">Subscribe</button>
             </form>
             
