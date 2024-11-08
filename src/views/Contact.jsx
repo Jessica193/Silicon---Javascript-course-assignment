bb02cf8498968f4e86d6f4ab52add638b8a075df
Jessica Eriksson <jessicahlqvist@hotmail.com>
2024-11-08 12:42:08 +0100

    html och css för address-section

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -77,18 +77,74 @@ const Contact = () => {
 
 
     <section className='address-section'>
-        <div className="container">
+        <div className="container address-container">
 
-            <div>
+            <div className='map'>
                 <img src="public\images\contact\map.svg" alt="" />
             </div>
 
-            <div className="adresses">
+            <div className="medical-centers">
 
-                <div className="adress">
+                <div className="medical-center">
                     <h5>Medical Center 1</h5>
+                    <div className='medical-center-contacts'>
+                        <img src="public\images\contact\pin-map.svg" alt="" />
+                        <p>4517 Washington Ave. Manchester, Kentucky 39495</p>
+                    </div>
+
+                    <div className='medical-center-contacts'>
+                        <img src="public\images\contact\phone.svg" alt="" />
+                        <p>(406) 555-0120</p>
+                    </div>
+
+                    <div className='medical-center-contacts'>
+                        <img src="public\images\contact\clock.svg" alt="" />
+                        <p>Mon – Fri: 9:00 am – 22:00 am <br />
+                        Sat – Sun: 9:00 am – 20:00 am</p>
+                    </div>
+                </div>
+
+                <div className="medical-center">
+                    <h5>Medical Center 2</h5>
+                    <div className='medical-center-contacts'>
+                        <img src="public\images\contact\pin-map.svg" alt="" />
+                        <p>2464 Royal Ln. Mesa,New Jersey 45463</p>
+                    </div>
+
+                    <div className='medical-center-contacts'>
+                        <img src="public\images\contact\phone.svg" alt="" />
+                        <p>(406) 544-0123</p>
+                    </div>
+
+                    <div className='medical-center-contacts'>
+                        <img src="public\images\contact\clock.svg" alt="" />
+                        <p>Mon – Fri: 9:00 am – 22:00 am <br />
+                        Sat – Sun: 9:00 am – 20:00 am</p>
+                    </div>    
+                </div>
+
+                <div className="social-media">
+
+                    <a className="social-media-icon">
+                        <img src="public\images\contact\facebook-icon.svg" alt="" />
+                    </a>
+
+                    <a className="social-media-icon">
+                        <img src="public\images\contact\twitter-icon.svg" alt="" />
+                    </a>
+
+                    <a className="social-media-icon">
+                        <img src="public\images\contact\instagram-icon.svg" alt="" />
+                    </a>
+
+                    <a className="social-media-icon">
+                        <img src="public\images\contact\youtube-icon.svg" alt="" />
+                    </a>
+
                 </div>
             </div>
+
+
 
         </div>
     </section>
