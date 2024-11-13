7d9bdbd64b23c9de781118f6b34f25dd421d5d2d
Jessica Eriksson <jessicahlqvist@hotmail.com>
2024-11-13 15:04:08 +0100

    fungerande mobilmeny på plats i header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import siliconLogoLight from '../assets/images/siliconLogo.svg'
 import siliconLogoDark from '../assets/images/siliconLogoDarkmode.svg'
 import iconSignIn from '../assets/images/icon-sign-in.svg'
@@ -7,11 +7,23 @@ import DarkmodeSwitch from './DarkmodeSwitch'
 import { Link } from 'react-router-dom'
 
 const Header = () => {
+
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    const toggleMenu = () => {
+        setIsExpanded (prev => !prev)
+    }
+
+    const closeMenu = () => {
+        setIsExpanded(false)
+    }
+
+
+
   return (
     <header>
         <div className="container header-container"> 
 
-            
             <Link id="logo" to="/">
                 <img className="silicon-logo-lightmode" src={siliconLogoLight} alt="silicon logotype" />
                 <img className="silicon-logo-darkmode" src={siliconLogoDark} alt="silicon logotype"/>
@@ -22,17 +34,24 @@ const Header = () => {
             </nav>
 
             <DarkmodeSwitch />
-        
-            
-
+    
             <a  className="btn-primary" id="auth-signin" href="#">
                 <img src={iconSignIn} alt="user icon"/>
                 <span>Sign in / up</span>
             </a>
 
-            <button className="btn-mobile">
+            <button onClick={toggleMenu} className="btn-mobile" aria-expanded={isExpanded ? 'true' : 'false'}>
                 <img src={mobileMenu} alt="mobile menu"/>
             </button>
+
+            <ul className='mobile-menu'>
+                <li>
+                    <Link onClick={closeMenu} className='mobile-menu-link' to={'/'}>Home</Link>
+                </li>
+                <li>
+                    <Link onClick={closeMenu} className='mobile-menu-link' to={'/contact'}>Contact</Link>
+                </li>
+            </ul>
         </div>
     </header>
   )
